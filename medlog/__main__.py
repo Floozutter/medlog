6693466c3658a7ec7a9d3d697b26d99cfcd2cb7c
Floozutter <floozutter@gmail.com>
2020-03-20 03:36:46 -0700

    Set up module structure

--- medlog/__main__.py
+++ medlog/__main__.py
@@ -1,2 +1,20 @@
 from argparse import ArgumentParser, ArgumentTypeError
 from pathlib import Path
+
+
+def parse_args() -> Path:
+    """
+    Get the text file path argument from the command line.
+    """
+    pass
+
+
+def main(logfilepath: Path) -> None:
+    """
+    Create and append a stringified Entry to a log file.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main(parse_args())
