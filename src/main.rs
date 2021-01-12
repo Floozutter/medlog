cdf4ee24ed02ec1bad1a1222c3666f328ef0e162
Floozutter <floozutter@gmail.com>
2021-01-12 04:14:37 -0800

    file -> log_file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs::{File, OpenOptions};
 use std::borrow::Cow;
 
 const USAGE: &str = "\
-usage: medlog <file> <dose_mg>
+usage: medlog <log_file> <dose_mg>
 example: medlog estradiol.log 2
 ";
 
@@ -11,15 +11,15 @@ fn parse_args() -> Result<(File, u32), Cow<'static, str>> {
     if args.next().is_none() {
         return Err("missing arguments!".into());
     }
-    let file = match args.next() {
-        Some(logfile) => logfile,
-        None => return Err("missing argument <file>!".into()),
+    let log_file = match args.next() {
+        Some(log_file) => log_file,
+        None => return Err("missing argument <log_file>!".into()),
     };
-    let file = match OpenOptions::new().append(true).create(true).open(file) {
-        Ok(file) => file,
+    let log_file = match OpenOptions::new().append(true).create(true).open(log_file) {
+        Ok(log_file) => log_file,
         Err(error) => {
             return Err(format!(
-                "can't open or create argument <file>! ({})",
+                "can't open or create argument <log_file>! ({})",
                 error
             ).into());
         },
@@ -37,17 +37,17 @@ fn parse_args() -> Result<(File, u32), Cow<'static, str>> {
             ).into());
         },
     };
-    Ok((file, dose_mg))
+    Ok((log_file, dose_mg))
 }
 
 fn main() {
-    let (file, dose_mg) = match parse_args() {
+    let (log_file, dose_mg) = match parse_args() {
         Ok(args) => args,
         Err(error) => {
             println!("{}\nerror: {}", USAGE, error);
             std::process::exit(1);
         },
     };
-    println!("*notices your args* owo what's this?: {:?} {}", file, dose_mg);
+    println!("*notices your args* owo what's this?: {:?} {}", log_file, dose_mg);
 }
 
